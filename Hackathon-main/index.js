f5b2defeea49c3a22e6746e7c99957145b69a5fa
Bibek Thakur <105601993+BibekThakur1@users.noreply.github.com>
2024-03-17 23:33:02 +0530

    Add files via upload

---
+++ Hackathon-main/index.js
@@ -0,0 +1,174 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+//for fan control
+app.post("/fan", async (req, res) => {
+  console.log(req.body);
+
+  //data to be sent through the API
+
+  const jsonData = {
+    teamid: "Kh14XK0",
+    device: `${req.body.device}`,
+    value: req.body.value,
+  };
+  let status = "ON";
+  if (req.body.value > 0) {
+    status = "OFF";
+  }
+
+  const statusData = {
+    fanSpeed: req.body.value,
+    fanStatus: status,
+  };
+
+  const apiUrl = "https://kodessphere-api.vercel.app/devices";
+
+  axios
+    .post(apiUrl, jsonData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  res.render("index.ejs", statusData);
+});
+
+
+//for bulb
+app.post("/bulb", async (req, res) => {
+  console.log(req.body);
+  //data to be sent through the API
+
+  const jsonData = {
+    teamid: "Kh14XK0",
+    device: `${req.body.device}`,
+    value: req.body.value,
+  };
+
+  let bulbStatus = "OFF";
+  if (req.body.value == 1) bulbStatus = "ON";
+  const statusData = {
+    bulb: bulbStatus,
+  };
+
+  const apiUrl = "https://kodessphere-api.vercel.app/devices";
+
+  axios
+    .post(apiUrl, jsonData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  res.render("index.ejs", { data: statusData });
+});
+
+//for  led 
+app.post("/LED", async (req, res) => {
+  console.log(req.body);
+
+  //data to be sent through the API
+  const jsonData = {
+    teamid: "Kh14XK0",
+    device: `${req.body.device}`,
+    value: req.body.value,
+  };
+
+  let ledStatus = "OFF";
+  if (req.body.value) ledStatus = "ON";
+  const statusData = {
+    led: ledStatus,
+    color: req.body.value,
+  };
+
+  const apiUrl = "https://kodessphere-api.vercel.app/devices";
+
+  axios
+    .post(apiUrl, jsonData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  res.render("index.ejs", statusData );
+});
+
+
+//for  AC 
+app.post("/ac", async (req, res) => {
+  console.log(req.body);
+
+  //data to be sent through the API
+  let temperatureAC = req.body.temp;
+  if(req.body.state === 0)
+  temperatureAC = 22;
+  const jsonData = {
+    teamid: "Kh14XK0",
+    device: `ac`,
+    value: {
+        temp: temperatureAC,
+        state: req.body.state
+    },
+  };
+
+  let acStatus = 'OFF';
+  if (req.body.state === '1') 
+    acStatus = "ON";
+
+  let statusData = {
+    ac: acStatus,
+    temperature: req.body.temp,
+  };
+
+  const apiUrl = "https://kodessphere-api.vercel.app/devices";
+
+  axios
+    .post(apiUrl, jsonData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  res.render("index.ejs", statusData );
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
